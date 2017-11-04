ac1f916319f9eb6f586b5b7061ea512533a745a1
Carter Koehler <carterleekoehler@gmail.com>
2017-11-03 20:31:27 -0500

    AVLNode and Structure

--- search-engine/avltree.hpp
+++ search-engine/avltree.hpp
@@ -1,10 +1,29 @@
 #ifndef AVLTREE_H
 #define AVLTREE_H
 
+#include "indexinterface.hpp"
 
-class AVLTree
+template <class T>
+class AVLTree : public IndexInterface<T>
 {
 private:
+    template<class T>
+    class AVLNode
+    {
+    private:
+        T data;
+        AVLNode<T> * left;
+        AVLNode<T> * right;
+        int height;
+
+    public:
+        AVLNode(const T& inputData, AVLNode * leftPtr = nullptr,
+                AVLNode * rightPtr = nullptr, int nodeHeight = 0)
+                : data(inputData), left(leftPtr), right(rightPtr),
+                height(nodeHeight) {}
+    };
+
+    AVLNode<T> * root;
 
 
 public:
