f06f1fdc2828efa97e798944775b2e90c119ef42
Carter Koehler <carterleekoehler@gmail.com>
2017-11-07 21:14:42 -0600

    IndexedItem Design

--- search-engine/indexedterm.cpp
+++ search-engine/indexedterm.cpp
@@ -11,6 +11,14 @@ IndexedTerm::IndexedTerm(std::string termIndex) : term(termIndex) {}
 //GETTERS AND SETTERS
 //===================
 
+//search for ID and return by reference
+    //find ID value (std::find_if)
+    //return pair with ID paired frequency and boolean
+
+
+//search for ID and return by reference
+    //identical logic to other search, but returns by value
+
 
 
 
@@ -19,9 +27,19 @@ IndexedTerm::IndexedTerm(std::string termIndex) : term(termIndex) {}
 //=========
 
 /**
+ * Equality operator checks keys (terms) for equality
+ */
+
+
+
+/**
  * Addition Assignment Operator adds question ID to list if it isn't there,
  * if it is present, adds frequency
  */
+    //first, check to make sure keys are the same, if not, throw big, scary error because something is seriously wrong
+    //searches in vector for ID and returns reference
+    //if search was successful, add frequency to that pair
+    //if search was unsuccessful, add ID with frequency to vector
 
 
 /**
--- search-engine/indexedterm.h
+++ search-engine/indexedterm.h
@@ -5,15 +5,19 @@
 #include <vector>
 #include <utility>
 #include <vector>
+#include <algorithm>
 
 class IndexedTerm
 {
 private:
     std::string term;
     std::vector<std::pair<int, int>> idWithFrequency;
+    //search for ID and return by reference
 
 public:
     IndexedTerm(std::string = "");
+    //search for ID and return by value
+    //opertor == just checks keys; worries about internal data later
     //operator += appends ID or increases frequency with given ID
     //stream insertion operator prints term with contents of
 };
